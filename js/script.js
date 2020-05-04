58c29c0e13f2fd81812ab23013c0ca160a921d70
楠欣士 <>
2020-05-04 18:32:45 +0900

    mvをコーディング

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(function () {
+  objectFitImages();
 
-
-
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  window.addEventListener("resize", () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
 });
