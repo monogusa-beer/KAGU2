79cc433d98873ea8f30119901dcfc53aa5bde841
楠欣士 <>
2020-05-04 20:41:29 +0900

    header完成

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function () {
   objectFitImages();
+
+  // ハンバーガーボタン
+  $("#js-hamburgerButton").click(function () {
+    $("body").toggleClass("is-navActive");
+    if ("false" == $(this).attr("aria-expanded")) {
+      $(this).attr("aria-expanded", true);
+    } else {
+      $(this).attr("aria-expanded", false);
+    }
+  });
 });
